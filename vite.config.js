2c63bf6ec26542baa49cb1663e435d8b67f93996
artiom mezheynikov <artiom.mezheynikov.work@gmail.com>
2025-03-19 20:49:48 +0300

    test autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
 
 export default {
 	base: '/positivus',
 	css: {
 		devSourcemap: true,
+		postcss: {
+			plugins: [
+				autoprefixer(),
+			],
+			sourceMap: true,
+		},
 		preprocessorOptions: {
 			scss: {
 				sourceMap: true,
